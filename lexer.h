6c46609ae68720a782261a89aab7bd3744142f06
Rohit Jain <32898962+Rohit2706@users.noreply.github.com>
2020-02-20 21:34:49 +0530

    Added function prototypes and documentation

--- lexer.h
+++ lexer.h
@@ -1,18 +1,30 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-typedef enum{PLUS, MINUS, MUL, DIV, LT, LE, GE, GT, EQ, NE, DEF, ENDDEF, COLON, RANGEOP, SEMICOL, COMMA, ASSIGNOP, SQBO, SQBC, BO, BC, ID, NUM, RNUM, ERROR} TOKEN;
+// TOKEN is an enum variable for possible tokens in the ERPLAG language
+typedef enum{PLUS, MINUS, MUL, DIV, LT, LE, GE, GT, EQ, NE, DRIVERDEF, DRIVERENDDEF, DEF, ENDDEF, COLON, RANGEOP, SEMICOL, COMMA, ASSIGNOP, SQBO, SQBC, BO, BC, ID, NUM, RNUM, ERROR, SUCCESS} TOKEN;
 
+// VALUE is a tagged union that stores the numeric value for lexemes of type NUM(tag=1) and RNUM(tage=2). For others VALUE is displayed as none(tag=0)
 typedef union{
 	uint num_value;
 	float rnum_value;
 }VALUE;
 
+// TokenInfo contains all the required information about the tokenized lexeme, to be returned to the parser on demand.
 typedef struct{
 	TOKEN token;
 	char lexeme[25];
-	uint length;
 	uint line_no;
+	int length;
 	VALUE value;
 	int tag;
 }TokenInfo;
+
+// Functions declarations for lexer
+TokenInfo getNextToken(char* buffer1, char* buffer2);
+TokenInfo tokenGen(TokenInfo newToken, TOKEN token_name);
+void getStream(char* buffer);
+TokenInfo failure(TokenInfo newToken, char ch);
+char getNextChar(char* buffer1, char* buffer2, char* lexeme, int* length);
+void retract(char* buffer1, char* buffer2, char* lexeme, int* length);
+TOKEN search_keyword(char* lexeme);
