153d710bc25ac8c985ce71f3dfa2dd3654231ad6
anirudh79 <33860645+anirudh79@users.noreply.github.com>
2020-02-22 18:35:06 +0530

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,5 +1,8 @@
 
-typedef stuct Stack{
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Stack{
     int capacity;
     int pos;
     char* elem;
@@ -9,11 +12,11 @@ stack* makeStack(int inpCapacity){
     stack* res=(stack*)malloc(sizeof(stack));
     res->capacity=inpCapacity;
     res->pos=-1;
-    res->elem=(char*)malloc(sizeof(char`)*inpCapacity);
+    res->elem=(char*)malloc(sizeof(char)*inpCapacity);
 }
 
 void push(stack* inpStack, char pushElem){
-    if(inpStack->pos<inpStack->capacity-1){
+    if(inpStack->pos<inpStack->capacity){
         inpStack->pos++;
         inpStack->elem[inpStack->pos]=pushElem;
     }
@@ -36,8 +39,12 @@ char pop(stack* inpStack){
     
 }
 
-
 int size(stack* inpStack){
     return inpStack->pos+1;
 }
+
+char top(stack* inpStack){
+	return inpStack->elem[inpStack->pos];
+}
+
 
